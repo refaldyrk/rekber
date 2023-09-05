e382ca270c2e2c99a796603b1fed1a6a50dc502f
Refaldy <refaldy.rizky22@gmail.com>
2023-09-05 15:59:58 +0700

    new(project)

---
+++ main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"rekber/config"
+	"rekber/handler"
+	"rekber/repository"
+	"rekber/service"
+	"syscall"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	//Set Context
+	ctx := context.Background()
+	//Init Viper
+	viper.SetConfigFile(".env")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	//Init Config
+	mongodb := config.ConnectMongo(ctx)
+	err = mongodb.Ping(1000)
+	if err != nil {
+		panic(err)
+	}
+
+	//Init Database
+	DB := mongodb.Database("rekber")
+
+	//=================> Repository
+	userRepo := repository.NewUserRepository(DB)
+
+	//=================> Service
+	userService := service.NewUserService(userRepo)
+	authService := service.NewAuthService(userRepo)
+
+	//=================> Handler
+	userHandler := handler.NewUserHandler(userService)
+	authHandler := handler.NewAuthHandler(authService)
+
+	//Server
+	app := gin.Default()
+
+	app.Use(gin.Recovery())
+	app.Use(gin.Logger())
+
+	// cors	config
+	cfg := cors.DefaultConfig()
+	cfg.AllowOrigins = []string{"*"}
+	cfg.AllowCredentials = true
+	cfg.AllowMethods = []string{"*"}
+	cfg.AllowHeaders = []string{"*"}
+
+	app.Use(cors.New(cfg))
+
+	//======================> Route
+
+	app.POST("/register", userHandler.Register)
+	app.POST("/login", authHandler.Login)
+
+	//Init Server
+	srv := &http.Server{
+		Addr:    ":9090",
+		Handler: app,
+	}
+
+	// graceful shutdown
+	go func() {
+		// service connections
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 4*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+
+	<-ctx.Done()
+
+	log.Println("Server exiting")
+}
