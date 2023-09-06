9ba5c41c2d12e946c477615e0fef75cb56ef25de
Refaldy <refaldy.rizky22@gmail.com>
2023-09-06 12:53:06 +0700

    add(constant for order)

---
+++ constant/order.go
@@ -0,0 +1,15 @@
+package constant
+
+const (
+	WAITING_STATUS  = "waiting"
+	PENDING_STATUS  = "pending"
+	SUCCESS_STATUS  = "success"
+	CANCELED_STATUS = "canceled"
+
+	//Role
+	BUYER  = "buyer"
+	SELLER = "seller"
+
+	//Minimum
+	MINIMUM_PRICE = 3000
+)
